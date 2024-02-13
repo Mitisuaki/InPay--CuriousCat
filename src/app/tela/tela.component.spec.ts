7ad193e0bd48846247b1f3219dd6cf956297a21d
unknown <lenysjunior@gmail.com>
2024-02-13 16:53:47 -0300

    Tela Usuario

---
+++ src/app/tela/tela.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TelaComponent } from './tela.component';
+
+describe('TelaComponent', () => {
+  let component: TelaComponent;
+  let fixture: ComponentFixture<TelaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TelaComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TelaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/tela/tela.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela',
+  standalone: true,
+  imports: [],
+  templateUrl: './tela.component.html',
+  styleUrl: './tela.component.css'
+})
+export class TelaComponent {
+
+}
