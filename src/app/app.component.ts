7f85b007af8367243e385fbaa256f07d6252d70a
IngridOguro <ingrid.shizue@gmail.com>
2024-02-17 15:05:23 -0300

    Componente criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HistoricoTransacoesComponent } from './historico-transacoes/historico-transacoes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HistoricoTransacoesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
