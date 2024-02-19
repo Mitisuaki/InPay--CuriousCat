c81d31c08823c3e2775102f5715dfa74d6e680af
Miti <mitisuaki@gmail.com>
2024-02-19 19:24:21 -0300

    Revert "test get CEP" because it was a sent on wrong branch
    This reverts commit 062b318599f658e1d181df6c149f0fb13e5da42a.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { CEPTestComponent } from './pages/cep-test/cep-test.component';
 
-export const routes: Routes = [
-  {path:"cep-test", component: CEPTestComponent},
-  {path:"", redirectTo: "/cep-test", pathMatch:"full"}
-];
+export const routes: Routes = [];
