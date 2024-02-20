71f498d629d8669b76a1b7d2d5ce61cbb9e6e4b4
Miti <mitisuaki@gmail.com>
2024-02-20 17:11:49 -0300

    rota para user/home-page adicionada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/user/home-page/home-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path:"user/home-page", component: HomePageComponent},
+  {path:"", redirectTo: "/home", pathMatch:"full"}];
