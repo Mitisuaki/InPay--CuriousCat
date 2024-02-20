1e672f1a56e11e3b3bda8824285be2614c8df189
jucezar <juliocezar.m.a@gmail.com>
2024-02-20 14:31:12 -0300

    Aplicando Image Optimization do Angulas nas Tags <img> e Movendo imagens para pasta padrao Assets

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,9 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
