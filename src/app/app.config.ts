01c9ceac5c74888dc210982a97d6906341813f28
Leandro Lopes <80966381+leandrolopes86@users.noreply.github.com>
2024-02-20 03:02:32 -0300

    incluido mascara nos campos, configurado recuperacao de senha, cadastro somente com campos preenchidos.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideEnvironmentNgxMask()]
 };
