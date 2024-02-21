0ae4d27c8c15a60500b07bd462bcff566bfe55f0
IS Oguro <ingrid.shizue@gmail.com>
2024-02-21 10:05:06 -0300

    tabela e  sidebar
    Tabela de transações.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HistoricoTransacoesComponent } from './historico-transacoes/historico-transacoes.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HistoricoTransacoesComponent],
+  imports: [CommonModule, RouterOutlet, HistoricoTransacoesComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/historico-transacoes/historico-transacoes.component.ts
+++ src/app/historico-transacoes/historico-transacoes.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Transaction {
+  transaction_id:number ,
+  nome:string,
+  data:string,
+  valor:number,
+  transaction_status: string,
+  acao:string
+}
 
 @Component({
   selector: 'app-historico-transacoes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './historico-transacoes.component.html',
   styleUrl: './historico-transacoes.component.css'
 })
 export class HistoricoTransacoesComponent {
+  // OPÇÕES DA SIDEBAR
+  menu_options:string[] = ['Dashboards','Transações',
+                          'Depósitos','Transferências',
+                          'Pagamentos','Sair'];
 
+  transactions:Array<Transaction> =[
+    {transaction_id:1 ,nome:'Jimin', data:'10/10/2010', valor:1000, transaction_status: 'Recebido', acao:'Detalhes' },
+    {transaction_id:1 ,nome:'Jin', data:'10/10/2010', valor:1000, transaction_status: 'Recebido', acao:'Detalhes' }
+  ];
 }
