dd5c147638991c50474de77c275efc4cef93fba5
Miti <mitisuaki@gmail.com>
2024-02-22 03:39:35 -0300

    Ajustado nome da pagina e icone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'InPay';
+  title = 'InPay | A fintch feita para você';
 }
