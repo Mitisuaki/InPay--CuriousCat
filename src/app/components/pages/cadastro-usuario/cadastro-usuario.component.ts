f57679a4932a24b586579b5582b05f0860e07ca0
IS Oguro <ingrid.shizue@gmail.com>
2024-03-19 17:04:13 -0300

    modal-termos funcionando

--- src/app/components/pages/cadastro-usuario/cadastro-usuario.component.ts
+++ src/app/components/pages/cadastro-usuario/cadastro-usuario.component.ts
@@ -5,13 +5,15 @@ import { Router } from '@angular/router';
 import { UserHomePageHeaderComponent } from "../user/header/header.component";
 import { UserHomePageFooterComponent } from "../footer/footer.component";
 import { HeaderComponent } from "../header/header.component";
+import { TermosDeUsoModalComponent } from '../termos-de-uso-modal/termos-de-uso-modal.component';
+
 
 @Component({
     selector: 'app-cadastro-usuario',
     standalone: true,
     templateUrl: './cadastro-usuario.component.html',
     styleUrl: './cadastro-usuario.component.css',
-    imports: [CommonModule, ReactiveFormsModule, UserHomePageHeaderComponent, UserHomePageFooterComponent, HeaderComponent]
+    imports: [CommonModule, ReactiveFormsModule, UserHomePageHeaderComponent, UserHomePageFooterComponent, HeaderComponent,TermosDeUsoModalComponent]
 })
 export class CadastroUsuarioComponent {
   // formulário
