3ebc86d114bae91cfd3796ef7dfa03976cae425f
IS Oguro <ingrid.shizue@gmail.com>
2024-03-20 18:21:04 -0300

    home-main seção inicial

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-
+import { NgOptimizedImage } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
