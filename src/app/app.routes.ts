15359bddba5085563f32d191ba1c2f53c2c5e717
jucezar <juliocezar.m.a@gmail.com>
2024-03-20 01:35:05 -0300

    Iniciando pagina CreditCardPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LandingPageComponent } from './components/pages/landing-page/landing-pa
 import { HomePageComponent } from './components/pages/user/home-page/home-page.component';
 import { HistoricoTransacoesComponent } from './components/pages/user/historico-de-transacoes/historico-transacoes/historico-transacoes.component';
 import { CadastroUsuarioComponent } from './components/pages/cadastro-usuario/cadastro-usuario.component';
+import { CartaoCreditoPageComponent } from './components/pages/user/cartao-credito-page/cartao-credito-page.component';
 
 
 export const routes: Routes = [
   {path:"user/home-page", component: HomePageComponent},
   {path:"user/transactions", component: HistoricoTransacoesComponent},
+  {path:"user/card-Page", component: CartaoCreditoPageComponent },
   {path:"cadastro", component: CadastroUsuarioComponent},
   {path:"", component: LandingPageComponent}
 ];
---
+++ src/app/components/pages/user/cartao-credito-page/cartao-credito-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cartao-credito-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './cartao-credito-page.component.html',
+  styleUrl: './cartao-credito-page.component.css'
+})
+export class CartaoCreditoPageComponent {
+
+}
