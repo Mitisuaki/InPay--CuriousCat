b7b62ce6ef26fda81d8345746fbd1fe11467da86
jucezar <juliocezar.m.a@gmail.com>
2024-03-22 00:38:25 -0300

    Criado componente -atividades-realizadas-component-

---
+++ src/app/components/pages/user/cartao-credito-page/atividades-realizadas-component/atividades-realizadas-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atividades-realizadas-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './atividades-realizadas-component.component.html',
+  styleUrl: './atividades-realizadas-component.component.css'
+})
+export class AtividadesRealizadasComponentComponent {
+
+}
