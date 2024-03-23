992916e3562def3da95f1c0dfb0bae3f8fa3d9ef
jucezar <juliocezar.m.a@gmail.com>
2024-03-23 00:07:01 -0300

    Fazendo componente modal para ajustar periodo de atividades

---
+++ src/app/components/pages/user/cartao-credito-page/modal-periodo-componente/modal-periodo-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-periodo-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-periodo-componente.component.html',
+  styleUrl: './modal-periodo-componente.component.css'
+})
+export class ModalPeriodoComponenteComponent {
+
+}
