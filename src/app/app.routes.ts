5c82ef4dfe77d7bdfa8bc121ec1412efe1e20f13
IS Oguro <ingrid.shizue@gmail.com>
2024-03-23 21:06:39 -0300

    layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { LandingPageComponent } from './components/pages/landing-page/landing-pa
 import { HomePageComponent } from './components/pages/user/home-page/home-page.component';
 import { HistoricoTransacoesComponent } from './components/pages/user/historico-de-transacoes/historico-transacoes/historico-transacoes.component';
 import { CadastroUsuarioComponent } from './components/pages/cadastro-usuario/cadastro-usuario.component';
-import { CriarAcessarContaComponent } from './components/pages/criar-acessar-conta/criar-acessar-conta.component';
+import { CriarAcessarContaComponent } from './components/pages/user/criar-acessar-conta/criar-acessar-conta.component';
 
 export const routes: Routes = [
   {path:"user/home-page", component: HomePageComponent},
   {path:"user/transactions", component: HistoricoTransacoesComponent},
   {path:"cadastro", component: CadastroUsuarioComponent},
   {path:"", component: LandingPageComponent},
-  {path:"user/acessar_conta",component: CriarAcessarContaComponent},
+  {path:"user/access-account",component: CriarAcessarContaComponent},
 ];
--- src/app/components/pages/user/criar-acessar-conta/criar-acessar-conta.component.ts
+++ src/app/components/pages/user/criar-acessar-conta/criar-acessar-conta.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { UserHeaderComponent } from '../user-header/user-header.component';
+import { UserHeaderComponent } from '../../user-header/user-header.component';
 
 @Component({
   selector: 'app-criar-acessar-conta',
