ed7aa9659bac751dda60dc48c25183c513cec897
IS Oguro <ingrid.shizue@gmail.com>
2024-03-25 18:17:12 -0300

    tabela de dados

--- src/app/components/pages/user/deposito/deposito.component.ts
+++ src/app/components/pages/user/deposito/deposito.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UserHeaderComponent } from '../user-header/user-header.component';
 
 @Component({
   selector: 'app-deposito',
   standalone: true,
-  imports: [],
+  imports: [UserHeaderComponent],
   templateUrl: './deposito.component.html',
   styleUrl: './deposito.component.css'
 })
